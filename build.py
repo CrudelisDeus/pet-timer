00dcecb45d43df3a5f5075197e709a8d75f30669
Dmitry <mathmainfunction@gmail.com>
2025-03-12 14:53:10 +0200

    added build.py

--- build.py
+++ build.py
@@ -3,3 +3,4 @@ import subprocess
 subprocess.run("pip freeze > requirements.txt", shell=True)
 subprocess.run("pyside6-uic ui/main.ui -o ui_main.py", shell=True)
 subprocess.run("pyside6-rcc rc.qrc -o rc_rc.py", shell=True)
+subprocess.run("pyinstaller --name 'Smart Sleep Timer' --windowed --onefile --specpath . main.py", shell=True)
